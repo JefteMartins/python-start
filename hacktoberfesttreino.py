88fc96b1d98d519674f97dd7e22a0b6576304356
JefteMartins <36806973+JefteMartins@users.noreply.github.com>
2021-10-29 12:24:59 -0300

    primeiro commit

---
+++ hacktoberfesttreino.py
@@ -0,0 +1,7 @@
+def verificaNumero():
+    num1 = input("Digite o primeiro numero: ")
+    num2 = input("Digite o segundo numero: ")
+    num3 = input("Digite o terceiro numero: ")
+    num1 = int(num1)
+    num2 = int(num2)
+    num3 = int(num3)
