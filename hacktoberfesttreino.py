de6d92a50f54bdb8d531168fe8a31eeb2a7e5f5a
JefteMartins <jefte.martins@hotmail.com>
2021-10-29 23:30:36 -0300

    Add files via upload

---
+++ hacktoberfesttreino.py
@@ -0,0 +1,9 @@
+def verificaNumero():
+    num1 = input("Digite o primeiro numero: ")
+    num2 = input("Digite o segundo numero: ")
+    num3 = input("Digite o terceiro numero: ")
+    num1 = int(num1)
+    num2 = int(num2)
+    num3 = int(num3)
+    print(num1,num2,num3)
+verificaNumero()
